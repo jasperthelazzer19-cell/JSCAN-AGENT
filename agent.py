8c7b10a8acf731423f373851086ff28b2c9a205e
JSCAN <jasperthelazzer19@gmail.com>
2026-04-27 09:00:32 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -6,6 +6,15 @@ import requests
 import schedule
 from datetime import datetime, timedelta
 from flask import Flask, render_template_string, request, jsonify
+
+app = Flask(__name__)
+
+@app.after_request
+def add_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
 import anthropic
 import sendgrid
 from sendgrid.helpers.mail import Mail
